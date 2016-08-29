c38dd72f6700c937d2032837f4eeb070bc73449a
Evgeny Smolovich <Evgenysm@trad.local>
2016-08-29 19:57:50 +0300

    fnishing calc page

--- app/front-assets/js/directives/toggleDescription.directive.js
+++ app/front-assets/js/directives/toggleDescription.directive.js
@@ -6,7 +6,7 @@ define(['angular', './directives'], function() {
             return {
                 restrict: 'A',
                 link: function(scope, element, attrs) {
-                    $(element).on('click', function(){
+                    $(element).on('click', function() {
                         $('.item').removeClass('opened');
                         $('.description').hide();
                         $(this).next('.description').fadeIn(400);
@@ -17,18 +17,43 @@ define(['angular', './directives'], function() {
             };
         });
 
-        angular
-            .module('schnizelApp.directives')
-            .directive('closeDescription', function($timeout) {
-                return {
-                    restrict: 'A',
-                    link: function(scope, element) {
-                        $(element).on('click', function(){
-                            $('.item').removeClass('opened');
-                            $('.description').slideUp(400);
-                        })
-                    }
-                };
-            });
+    angular
+        .module('schnizelApp.directives')
+        .directive('closeDescription', function($timeout) {
+            return {
+                restrict: 'A',
+                link: function(scope, element) {
+                    $(element).on('click', function() {
+                        $('.item').removeClass('opened');
+                        $('.description').slideUp(400);
+                    })
+                }
+            };
+        });
+
+    angular
+        .module('schnizelApp.directives')
+        .directive('effectIfInView', function($timeout) {
+            return {
+                restrict: 'A',
+                link: function(scope, element, attrs) {
+                    $(window).on('scroll', function(){
+                        var pageTop = $(window).scrollTop();
+                        var pageBottom = pageTop + $(window).height();
+                        var elementTop = $(element).offset().top;
+                        var elementBottom = elementTop + $(element).height();
+
+                        console.log(pageTop, elementTop, pageBottom, elementBottom);
+
+                        if((pageTop < elementTop) && (pageBottom > elementBottom)){
+                            $('.'+attrs.effectIfInView).addClass('staticView');
+                        }
+                        else {
+                            $('.'+attrs.effectIfInView).removeClass('staticView');
+                        }
+                    });
+                }
+            };
+        });
 
 });
