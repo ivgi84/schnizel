621c23c5fefbd2fc474fa89d2618f8aec3de0e4e
Evgeny <ivgi84@gmail.com>
2016-10-05 18:37:03 +0300

    update2

--- app/front-assets/js/components/storyPage/storyPageCmp.js
+++ app/front-assets/js/components/storyPage/storyPageCmp.js
@@ -1,6 +1,6 @@
-define(['angular', './components/components', 'tweenMax', 'ScrollMagic', 'animationgsap','addIndicators'], 
+define(['angular', './components/components', 'tweenMax', 'ScrollMagic', 'animationgsap','addIndicators'],
   function(angular, components, tweenMax, ScrollMagic, animationgsap, addIndicators) {
-  
+
     angular
         .module('schnizelApp.components')
         .component('storyPage', {
@@ -13,21 +13,29 @@ define(['angular', './components/components', 'tweenMax', 'ScrollMagic', 'animat
 
         this.$postLink = function(){
 
-            var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "200%"}});
+            var controller = new ScrollMagic.Controller();
+
+            var scene = new ScrollMagic.Scene({
+									triggerElement: "#logo-trigger"
+								})
+								.setTween("#s-logo", 0.3, { scale: 1.3})
+								.addIndicators({name: "logo encrease"})
+								.addTo(controller);
 
   // build scenes
-  new ScrollMagic.Scene({triggerElement: "#parallax1"})
-          .setTween("#parallax1 > div", {y: "80%", ease: Linear.easeNone})
+  new ScrollMagic.Scene({triggerElement: "#parallax1", duration: 1000})
+          .setTween("#parallax1 > img", {top: window.innerHeight+'px'})
+          .addIndicators({name: "set 1"})
           .addTo(controller);
-
-  new ScrollMagic.Scene({triggerElement: "#parallax2"})
-          .setTween("#parallax2 > div", {y: "80%", ease: Linear.easeNone})
-          .addTo(controller);
-
-  new ScrollMagic.Scene({triggerElement: "#parallax3"})
-          .setTween("#parallax3 > div", {y: "80%", ease: Linear.easeNone})
-          .addTo(controller);
-    
+  //
+  // new ScrollMagic.Scene({triggerElement: "#parallax2"})
+  //         .setTween("#parallax2 > div", {y: "80%", ease: Linear.easeNone})
+  //         .addTo(controller);
+  //
+  // new ScrollMagic.Scene({triggerElement: "#parallax3"})
+  //         .setTween("#parallax3 > div", {y: "80%", ease: Linear.easeNone})
+  //         .addTo(controller);
+  //
 
 
         }
