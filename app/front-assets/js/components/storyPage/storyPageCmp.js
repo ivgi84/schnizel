444d5af73cd04272936b50221a9e5cb018ca57eb
Evgeny Smolovich <evgenysm@GLB-DG-05.trad.local>
2016-11-29 11:11:11 +0200

    update

--- app/front-assets/js/components/storyPage/storyPageCmp.js
+++ app/front-assets/js/components/storyPage/storyPageCmp.js
@@ -1,42 +1,57 @@
 define(['angular', 'TweenMax', 'ScrollMagic', 'animationgsap'],
-  function(angular, TweenMax, ScrollMagic, animationgsap) {
+    function(angular, TweenMax, ScrollMagic, animationgsap) {
 
-    function StoryPageCtrl() {
+        function StoryPageCtrl() {
 
-        this.$postLink = function(){
+            this.$postLink = function() {
 
-          var screenContents = $('.screenContent');
+                var videoTags = document.getElementsByTagName('video') || [];
+                angular.forEach(videoTags, function(video, ind){
+                    video.play();
+                    video.addEventListener('click', function() {
+                        video.play();
+                    }, false);
+                })
 
-          var count = screenContents.length;
-          var height = screenContents.innerHeight() * count;
+                var screenContents = $('.screenContent');
 
-          $('.images').css({
-            'height': height+'px',
-            'top':'-'+ height+'px'
-          });
+                var count = screenContents.length;
+                var height = screenContents.innerHeight() * count;
 
-          var duration = height;
+                $('.images').css({
+                    'height': height + 'px',
+                    'top': '-' + height + 'px'
+                });
 
+                var duration = height;
 
-            var controller = new ScrollMagic.Controller();
+                var controller = new ScrollMagic.Controller();
 
-            var scene = new ScrollMagic.Scene({
-									triggerElement: "#logo-trigger"
-								})
-								.setTween("#s-logo", 0.3, { scale: 1.3})
-								.addTo(controller);
+                var scene = new ScrollMagic.Scene({
+                        triggerElement: "#logo-trigger"
+                    })
+                    .setTween("#s-logo", 0.3, {
+                        scale: 1.3
+                    })
+                    .addTo(controller);
 
-  // build scenes
+                // build scenes
 
-  new ScrollMagic.Scene({triggerElement: "#logo-trigger", duration: duration})
-          .setTween("#images", {top: 1500 +'px'})
-          .addTo(controller);
+                new ScrollMagic.Scene({
+                        triggerElement: "#logo-trigger",
+                        duration: duration
+                    })
+                    .setTween("#images", {
+                        top: 1500 + 'px'
+                    })
+                    .addTo(controller);
+            };
+
+        }
+
+        return {
+            templateUrl: 'front-assets/js/components/storyPage/storyPageCmp.view.html',
+            controller: StoryPageCtrl
         };
-    }
 
-    return {
-        templateUrl: 'front-assets/js/components/storyPage/storyPageCmp.view.html',
-        controller: StoryPageCtrl
-    };
-
-});
+    });
